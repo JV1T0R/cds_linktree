f3a68882754093bc3b870859ebfb7b2c51d8f186
JV1T0R <jvesteves45@gmail.com>
2024-12-31 18:55:17 -0300

    first commit

---
+++ public/main.js
@@ -0,0 +1,17 @@
+function gotoLink(Link){
+    window.open(Link.value);
+};
+
+function copyPixKey() {
+    const PixKey = "cpamponet59@gmail.com";
+    navigator.clipboard.writeText(PixKey).then(() => {
+        const alertCopy = document.getElementById('alert-copy');
+        alertCopy.classList.remove('d-none');
+        setTimeout(() => {
+            alertCopy.classList.add('d-none');
+        }, 3000);
+    }).catch(err => {
+        console.error("Erro ao copiar a chave Pix: ", err);
+    });
+}
+
