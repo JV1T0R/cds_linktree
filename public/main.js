097f7e91eea722c2eea24fe6c443e148a81948bf
JV1T0R <jvesteves45@gmail.com>
2025-02-13 16:59:06 -0300

    style adjustments

--- public/main.js
+++ public/main.js
@@ -2,14 +2,25 @@ function gotoLink(link){
     window.open(link.value);
 };
 
+document.addEventListener("DOMContentLoaded", function () {
+    const copiarChaveBtn = document.getElementById("copiarChave");
+
+    if (copiarChaveBtn) {
+        copiarChaveBtn.addEventListener("click", copyPixKey);
+    }
+});
+
 function copyPixKey() {
     const PixKey = "cpamponet59@gmail.com";
     navigator.clipboard.writeText(PixKey).then(() => {
-        const alertCopy = document.getElementById('alert-copy');
-        alertCopy.classList.remove('d-none');
-        setTimeout(() => {
-            alertCopy.classList.add('d-none');
-        }, 3000);
+        const alertCopy = document.getElementById("alert-copy");
+
+        if (alertCopy) {
+            alertCopy.classList.remove("d-none");
+            setTimeout(() => {
+                alertCopy.classList.add("d-none");
+            }, 3000);
+        }
     }).catch(err => {
         console.error("Erro ao copiar a chave Pix: ", err);
     });
