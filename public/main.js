96b27d2935056fd609a6c3786ce3521f33bc40e8
João Vítor Pamponet Esteves <jvticontato@gmail.com>
2025-04-29 12:40:30 -0300

    General style adjustments

--- public/main.js
+++ public/main.js
@@ -1,6 +1,16 @@
-function gotoLink(link){
-    window.open(link.value);
-};
+// Function to redirect to the links
+document.addEventListener('DOMContentLoaded', function() {
+    const linkButtons = document.querySelectorAll('.link-btn')
+    
+    linkButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            const url = this.getAttribute('data-url')
+            if (url) {
+                window.open(url);
+            }
+        })
+    })
+})
 
 document.addEventListener("DOMContentLoaded", function () {
     const copiarChaveBtn = document.getElementById("copiarChave");
